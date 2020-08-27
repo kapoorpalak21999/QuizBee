d61283b43c8404b36ad639aa23c6a50a32e95477
Palak kapoor <63863056+kapoorpalak21999@users.noreply.github.com>
2020-08-27 20:40:22 +0530

    Add files via upload

---
+++ build/precache-manifest.91ee7bfb95ffe2992b6d4f9373eae295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50afbf037408d2e4941b990719124d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44b266dfe6703f59a331",
+    "url": "/static/css/main.3fff326a.chunk.css"
+  },
+  {
+    "revision": "431e44eca0f0169fbbc7",
+    "url": "/static/js/2.9f25e938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f25e938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b266dfe6703f59a331",
+    "url": "/static/js/main.c0346cb8.chunk.js"
+  },
+  {
+    "revision": "44bfa817bc26e1d657ac",
+    "url": "/static/js/runtime-main.14514217.js"
+  }
+]);
